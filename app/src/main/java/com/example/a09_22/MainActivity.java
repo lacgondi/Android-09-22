91426aa4ebdaaf790503439ab705dc4c4752cd91
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-09-22 14:20:04 +0200

    Add functions toÃ buttons and handel in and outputs

--- app/src/main/java/com/example/a09_22/MainActivity.java
+++ app/src/main/java/com/example/a09_22/MainActivity.java
@@ -2,18 +2,24 @@ package com.example.a09_22;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.Locale;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     RelativeLayout layout;
     EditText input;
-    Button b1;
-    Button b2;
-    Button b3;
+    TextView output;
+    Button toUpper;
+    Button toLower;
+    Button changeColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +27,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+        toUpper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String i = String.valueOf(input.getText()).toUpperCase();
+                output.setText(i);
+            }
+        });
+        toLower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String i = String.valueOf(input.getText()).toLowerCase();
+                output.setText(i);
+            }
+        });
+        changeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rand = new Random();
+                Random rnd = new Random();
+                output.setBackgroundColor(Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
+            }
+        });
     }
 
     public void init(){
         layout = findViewById(R.id.rel);
         input = findViewById(R.id.input);
-        b1 = findViewById(R.id.button1);
-        b2 = findViewById(R.id.button2);
-        b3 = findViewById(R.id.button3);
+        toUpper = findViewById(R.id.button1);
+        toLower = findViewById(R.id.button2);
+        changeColor = findViewById(R.id.button3);
+        output = findViewById(R.id.output);
     }
 }
